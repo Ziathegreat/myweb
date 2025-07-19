6979850641e41b960536485f40dfabde40ed4950
Ziathegreat <64564670+Ziathegreat@users.noreply.github.com>
2025-07-19 09:33:56 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const quotes = [
   "“Stay hungry, stay foolish.” – Steve Jobs",
   "“Code is like humor. When you have to explain it, it’s bad.” – Cory House",
   "“Simplicity is the soul of efficiency.” – Austin Freeman",
-  "“The best error message is the one that never shows up.” – Thomas Fuchs"
+  "“The best error message is the one that never shows up.” – Thomas Fuchs",
+  "“Axioms are accepted statements that we do not challenge and the make everything else true while not questioning them do not make them true.” -zia",
+  " “There nothing as justified rational beliefs because axioms are unjustified themselves to begin with and justification need other axioms to make justification just” -zia"
+  
 ];
 
 function getRandomQuote() {
